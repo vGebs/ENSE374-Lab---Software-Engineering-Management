de352f0bcb479a971995d6ca809ecc9d02639ca1
vGebs <vaughn.geber33@gmail.com>
2021-10-18 15:00:45 -0600

    complete login/signup

--- Lab6/index.js
+++ Lab6/index.js
@@ -1,28 +1,65 @@
 //imports
+const authService = require('./services/authService')
 const express = require("express")
 const app = express()
-
 const port = 3000
 
 //serve static files
 app.use(express.static('public'))
-// app.use(express.static(__dirname + 'public/css'))
-// app.use(express.static(__dirname + 'public/js'))
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+
+// MARK: - GET ===========================================================================================>
 
 //Send login/signup
-app.get('/', (req, res) =>
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
-)
+})
 
 //Send todo
-app.get('/todo', (req, res) =>
+app.get('/todo', (req, res) => {
     res.sendFile(__dirname + "/views/todo.html")
-)
+})
 
 //Form files not recognized
-app.get('*', (req, res) =>
+app.get('*', (req, res) => {
     res.send("Error 404: Page not found")
-);
+})
+
+
+// MARK: - POST ===========================================================================================>
+
+//login form
+app.post('/login', (req, res) => {
+    if (!req.body.email || !req.body.password) {
+        console.log("Form must not be empty")
+    } else {
+        if (authService.validateLogin(req.body.email, req.body.password)) {
+            res.redirect('/todo')
+        } else {
+            res.redirect('/')
+        }
+    }
+})
+
+//signup form
+app.post('/signup', (req, res) => {
+    if (!req.body.email || !req.body.password || !req.body.auth) {
+        console.log("Form must not be empty")
+    } else {
+        if (authService.validateSignup(req.body.email, req.body.password, req.body.auth)) {
+            res.redirect('/todo')
+        } else {
+            res.redirect('/')
+        }
+    }
+})
+
+
+// MARK: - Port Listener ================================================================================>
 
 //Set up sever
 app.listen(port, () => console.log('Server is listening at port: ' + port))
